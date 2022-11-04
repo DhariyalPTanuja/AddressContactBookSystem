19a4a5fd7fd7f46fa0ed6db39e6a283380891b5a
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-11-04 05:39:01 +0530

    Uc1 to create a Contacts in Address

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -8,6 +8,20 @@ import java.util.Scanner;
 public class AddressBookMain {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome in Address Book System!!");
-	}
+		Collection<PersonalInfo> contactBook = new ArrayList<PersonalInfo>();
+		Scanner scanI = new Scanner(System.in);
+		Scanner scanS = new Scanner(System.in);
+		
+		 contactBook.add(new PersonalInfo("Rahul", "Roy", 9812345675l, "rahulroy20@xyz.com", "Bangalore", "Karnataka", 563322));
+	        contactBook.add(new PersonalInfo("Lavanya", "Dhariyal", 970047875l, "lavanya2@abc.com", "Pune", "Maharashtra", 441222));
+	        contactBook.add(new PersonalInfo("Sachin", "Bhatt", 9812345875l, "sachinbhatt1@xyz.com", "Mangalore", "Karnataka", 563322));
+	        contactBook.add(new PersonalInfo("Aditya", "Agarwal", 9812347875l, "agarwal_aditya@abc.com", "Mathura", "UttarPradesh", 441222));
+
+	        Iterator<PersonalInfo> personalInfoIterator = contactBook.iterator();
+
+	        while (personalInfoIterator.hasNext()) {
+	            System.out.println(personalInfoIterator.next());
+	        }
+	
+}
 }
